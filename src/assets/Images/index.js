ab928a6a36339712172031dc5cc11755ac67e6ed
Johan Sierra <johansierra222@gmail.com>
2025-01-31 12:27:03 -0500

    feat: Add category images and create index.js - Index.js contains an array (called categories), of objects that can be mapped Each object contains a category name, description, and image URL - Category images are stored in the assets/Images folder - Each image is named after the category name (e.g. electronics.jpg) - The images are used in the index.js file to display the category images

---
+++ src/assets/Images/index.js
@@ -0,0 +1,17 @@
+import categ_streetKings from "./categ_streetKings.avif";
+import categ_tees from "./categ_tees.avif";
+import categ_jerseys from "./categ_jerseys.avif";
+import categ_shorts from "./categ_shorts.avif";
+import categ_sneakers from "./categ_sneakers.avif";
+import categ_accesories from "./categ_accesories.avif";
+
+const categories = [
+    { id: 1, title: "Street Kings", src: categ_streetKings },
+    { id: 2, title: "Tees", src: categ_tees },
+    { id: 3, title: "Jerseys", src: categ_jerseys },
+    { id: 4, title: "Shorts", src: categ_shorts },
+    { id: 5, title: "Sneakers", src: categ_sneakers },
+    { id: 6, title: "Accessories", src: categ_accesories }
+];
+
+export default categories;
