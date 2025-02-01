4ee8870acf9ee0ca0c38330e52247470227b1be5
Johan Sierra <johansierra222@gmail.com>
2025-02-01 07:11:45 -0500

    feat: Create reusable ProductCard component
    - Designed a styled container for the product card using styled-components.
    - Added props for product title, price, and image for flexibility.
    - Ensures proper styling and structure for a clean UI.

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,8 @@ import TopBarInfo from "./components/TopBarInfo";
 import NavBar from "./components/NavBar";
 import Hero from "./components/Hero";
 import CategoriesList from "./components/CategoriesList";
+import ProductCard from "./components/ProductCard";
+import { products } from "./assets/Images";
 
 function App() {
   return (
@@ -11,6 +13,16 @@ function App() {
       <NavBar />
       <Hero />
       <CategoriesList />
+      <div style={{width: '100%', display: 'flex',justifyContent:"center", position: 'relative', zIndex: -1, background: 'white', padding: '3rem 2rem'}}>
+        {products.map((product) => (
+          <ProductCard
+            key={product.id}
+            productTitle={product.name}
+            price={product.price}
+            srcImage={product.imageSrc}
+          />
+        ))}
+      </div>
     </>
   );
 }
---
+++ src/components/ProductCard.jsx
@@ -0,0 +1,50 @@
+import React from "react";
+import styled from "styled-components";
+
+const Container = styled.div`
+  display: flex;
+  flex-direction: column;
+  width: 15.6rem;
+  height: 100%;
+  border: 1px solid var(--greyE);
+`;
+const ProductImage = styled.img`
+  width: 100%;
+  height: 18.31rem;
+  object-fit: contain;
+`;
+
+const ProductInfo = styled.div`
+color: var(--secondary);
+    text-wrap: nowrap;
+  display: flex;
+  gap: 0.25rem;
+  flex-direction: column;
+  align-items: start;
+  padding: 1.25rem 1.5rem;
+`;
+
+const ProductTitle = styled.h5`
+  font-size: 1.1rem;
+  font-variation-settings: "wght" 600;
+  margin: 0;
+`;
+
+const Price = styled.p`
+  font-size: 0.8rem;
+  margin: 0;
+`;
+
+const ProductCard = ({ productTitle, price, srcImage }) => {
+  return (
+    <Container>
+      <ProductImage src={srcImage} />
+      <ProductInfo>
+        <ProductTitle>{productTitle}</ProductTitle>
+        <Price>${price}</Price>
+      </ProductInfo>
+    </Container>
+  );
+};
+
+export default ProductCard;
