f7845bbb37bfe04ccdc6cc1e171a32269631c2fd
Johan Sierra <johansierra222@gmail.com>
2025-02-02 17:05:08 -0500

    feat: add ProductsPage component with filtering and product display
    - Created ProductsPage component to list all products
    - Implemented a filter button for future filtering functionality
    - Styled page using styled-components
    - Integrated ProductCard component for displaying individual products

---
+++ src/pages/ProductsPage.jsx
@@ -0,0 +1,93 @@
+import React from "react";
+import ProductCard from "../components/ProductCard";
+import { products } from "../assets/Images";
+import styled from "styled-components";
+import { HiOutlineAdjustmentsHorizontal } from "react-icons/hi2";
+
+const Container = styled.main`
+  position: relative;
+  width: 100%;
+  background: var(--white);
+  color: var(--secondary);
+  padding: 1rem 2rem;
+`;
+
+const Heading = styled.h1`
+  font-size: 5rem;
+  margin-bottom: 0;
+  white-space: nowrap;
+`;
+
+const FilterAndResults = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+  position: sticky;
+  top: 84px;
+  left: 0;
+  background: var(--white);
+  padding: 1rem;
+`;
+
+const FilterButton = styled.button`
+  display: flex;
+  font-size: 1rem;
+  font-variation-settings: "wght" 500;
+  white-space: nowrap;
+  align-items: center;
+  gap: 4px;
+  border: 1px solid var(--greyE);
+  border-radius: 8px;
+  background: var(--white);
+  padding: 8.5px 11px;
+  color: var(--secondary);
+  cursor: pointer;
+  transition: background 0.5s ease;
+
+  &:hover {
+    background: var(--greyD);
+    transition: background 0.5s ease;
+  }
+`;
+
+const ResultsText = styled.p`
+  font-size: .8rem;
+`
+const ProductCardsContainer = styled.section`
+  margin-top: 2rem;
+  width: 100%;
+  display: flex;
+  flex-wrap: wrap;
+  align-items: center;
+  justify-content: center;
+  gap: 1rem;
+`;
+
+const ProductsPage = () => {
+  return (
+    <Container>
+      <Heading>All Products</Heading>
+
+      <FilterAndResults>
+        <FilterButton>
+          Filters
+          <HiOutlineAdjustmentsHorizontal />
+        </FilterButton>
+        <ResultsText>[{products.length} Results]</ResultsText>
+      </FilterAndResults>
+
+      <ProductCardsContainer>
+        {products.map((product) => (
+          <ProductCard
+            key={product.id}
+            productTitle={product.name}
+            price={product.price}
+            srcImage={product.imageSrc}
+          />
+        ))}
+      </ProductCardsContainer>
+    </Container>
+  );
+};
+
+export default ProductsPage;
