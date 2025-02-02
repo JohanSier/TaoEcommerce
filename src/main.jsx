f7b1d594d48014de296a76f0a331c1b59fd1e457
Johan Sierra <johansierra222@gmail.com>
2025-02-02 15:34:32 -0500

    feat: Modify main.jsx to fit router and Create ScrollToTop - ScrollToTop components works to redirect to the top when clicking a link in this case the footer logo is using the functionality

--- src/main.jsx
+++ src/main.jsx
@@ -1,9 +1,14 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './App.jsx'
+import {BrowserRouter} from "react-router"
+import ScrollToTop from './components/ScrollToTop.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    <BrowserRouter>
+        <ScrollToTop />
+        <App />
+    </BrowserRouter>
   </StrictMode>,
 )
