98a305a46eed5397d4b0ac947a5c8a67b59d4d79
Johan Sierra <johansierra222@gmail.com>
2025-02-23 16:31:25 -0500

    feat(ProductDetails): add size selection state and reset on route change
    - Implemented state management for size selection in `ProductDetails`
    - Allowed users to toggle size selection on click (deselect if clicked again)
    - Ensured size resets when navigating to a different product page
    - Fixed condition in `productsMayLike` to exclude the currently viewed product
    - Formatted file with prettier for better legibility

--- src/pages/SpecificProductTemplate.jsx
+++ src/pages/SpecificProductTemplate.jsx
@@ -1,9 +1,9 @@
-import React from 'react'
-import ProductCard from '../components/ProductCard'
-import ProductDetails from '../components/ProductDetails';
+import React from "react";
+import ProductCard from "../components/ProductCard";
+import ProductDetails from "../components/ProductDetails";
 import { products } from "../assets/Images";
-import styled from 'styled-components';
-import { useParams } from 'react-router';
+import styled from "styled-components";
+import { useParams } from "react-router";
 
 const Container = styled.main`
   position: relative;
@@ -16,21 +16,22 @@ const Container = styled.main`
 const Wrapper = styled.section`
   display: flex;
   position: relative;
-`
+`;
 
 const Grid = styled.div`
   width: 70%;
   flex-grow: 2;
   display: grid;
   grid-template-columns: 1fr 1fr;
-`
+`;
 
 const StyledImage = styled.img`
   width: 100%;
   height: 700px;
   object-fit: cover;
   object-position: center;
-`
+  flex-grow: 1;
+`;
 
 const MayAlsoLike = styled.section`
   display: flex;
@@ -39,7 +40,7 @@ const MayAlsoLike = styled.section`
   justiy-content: center;
   text-align: center;
   padding: 2rem;
-`
+`;
 
 const Heading = styled.h2`
   position: relative;
@@ -69,50 +70,52 @@ const Products = styled.div`
 
 const StyledProductDetails = styled(ProductDetails)`
   width: 100%;
-`
+`;
 const SpecificProductTemplate = () => {
-    const productsMayLike = products.slice(0,4)
-    const { id } = useParams(); 
-    const product = products.find((p) => p.id === parseInt(id)); 
-  
-    if (!product) {
-      return <h1>Producto no encontrado</h1>; // Manejo de error si el producto no existe
-    }
+  const productsMayLike = products.slice(0, 5);
+  const { id } = useParams();
+  const product = products.find((p) => p.id === parseInt(id));
+
+  if (!product) {
+    return <h1>Producto no encontrado</h1>; // Manejo de error si el producto no existe
+  }
 
   return (
-    <Container>     
+    <Container>
       <Wrapper>
         <Grid>
-        <StyledImage src={product.images[0]} alt={product.name} />
-          <StyledImage src={"https://static.bershka.net/assets/public/c116/0c9b/34894f2b9e8b/3443ef3e7f3f/02878538700-a2d/02878538700-a2d.jpg?ts=1738932442246&w=800"}/>
-          <StyledImage src={"https://static.bershka.net/assets/public/c116/0c9b/34894f2b9e8b/3443ef3e7f3f/02878538700-a2d/02878538700-a2d.jpg?ts=1738932442246&w=800"}/>
-          <StyledImage src={"https://static.bershka.net/assets/public/c116/0c9b/34894f2b9e8b/3443ef3e7f3f/02878538700-a2d/02878538700-a2d.jpg?ts=1738932442246&w=800"}/>
-          <StyledImage src={"https://static.bershka.net/assets/public/c116/0c9b/34894f2b9e8b/3443ef3e7f3f/02878538700-a2d/02878538700-a2d.jpg?ts=1738932442246&w=800"}/>
-          <StyledImage src={"https://static.bershka.net/assets/public/c116/0c9b/34894f2b9e8b/3443ef3e7f3f/02878538700-a2d/02878538700-a2d.jpg?ts=1738932442246&w=800"}/>
+          {products[id - 1].images.map((image, index) => (
+            <StyledImage key={index} src={image} alt={product.name} />
+          ))}
         </Grid>
 
-      <StyledProductDetails title={product.name} price={product.price} sizes={product.availableSizes} />
+        <StyledProductDetails
+          title={product.name}
+          price={product.price}
+          sizes={product.availableSizes}
+        />
       </Wrapper>
 
-
       <MayAlsoLike>
-            <Heading>You may also like</Heading>
-            <Products>
-            {productsMayLike.map((product) => (
-            <ProductCard
-                style={{flexGrow: "2"}}
+        <Heading>You may also like</Heading>
+        <Products>
+          {productsMayLike
+            .filter((p) => p.id !== parseInt(id)) // Exclude the current product
+            .map((product) => (
+              <ProductCard
+                style={{ flexGrow: "2" }}
                 key={product.id}
                 productTitle={product.name}
                 price={product.price}
                 srcImage={product.images[0]}
                 hoverImage={product.images[1]}
                 productLink={`/product/${product.id}`}
-            />
+              />
             ))}
-            </Products>
+        </Products>
       </MayAlsoLike>
     </Container>
-  )
-}
+  );
+};
 
 export default SpecificProductTemplate;
