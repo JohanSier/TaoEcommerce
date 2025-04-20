6986953f85566fdd0d0c036e1d201b126599e25f
Johan Sierra <johansierra222@gmail.com>
2025-04-19 22:27:30 -0500

    fix(Homepage): ensure Street Kings slider displays the correct products
    - Ensure Street Kings slider displays the just the products of Street Kings (with id 1)

--- src/pages/Homepage.jsx
+++ src/pages/Homepage.jsx
@@ -7,7 +7,7 @@ import { products } from "../assets/Images";
 const Homepage = () => {
   // Filtrar productos para mostrar solo los de Street Kings (categorías 2, 3 y 4 - Tees, Jerseys y Shorts)
   const streetKingsProducts = products.filter(product => 
-    product.categories.some(category => [2, 3, 4].includes(category.id))
+    product.categories.some(category => [1].includes(category.id))
   );
 
   return (
